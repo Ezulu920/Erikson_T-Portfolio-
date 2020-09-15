c33d5b94a41ba808c1c97574428a7af1507f8986
PaaTay2.0 <macbookpro@macbook-pro.myfiosgateway.com>
2020-09-15 04:35:16 -0400

    completed portfolio project

---
+++ JS/index.js
@@ -0,0 +1,73 @@
+const selectElement = (s) => document.querySelector(s);
+const navLinks = document.querySelectorAll(".nav-link");
+
+selectElement(".burger-menu-icon").addEventListener("click", () => {
+    selectElement(".nav-list").classList.toggle("active");
+    selectElement(".burger-menu-icon").classList.toggle("toggle")
+
+    navLinks.forEach((link, index) => {
+        if (link.style.animation) {
+            link.style.animation = ""
+        } else {
+            link.style.animation = `navLinkAnimate 0.5s ease forwards ${index / 7 + 0.5}s`
+            console.log(index / 7 + 0.5)
+        }
+    })
+});
+
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        selectElement(".nav-list").classList.toggle("active");
+        selectElement(".burger-menu-icon").classList.toggle("toggle");
+
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ""
+            } else {
+                link.style.animation = `navLinkAnimate 0.5s ease forwards ${index / 7 + 0.5}s`
+                console.log(index / 7 + 0.5)
+            }
+        })
+    })
+})
+
+
+const navbar = document.querySelector("#nav");
+const navBtn = document.querySelector("#nav-btn");
+const closeBtn = document.querySelector("#close-btn");
+const sidebar = document.querySelector("#sidebar");
+const date = document.querySelector("#date");
+// add fixed class to navbar
+window.addEventListener("scroll", function () {
+    if (window.pageYOffset > 80) {
+        navbar.classList.add("navbar-fixed");
+    } else {
+        navbar.classList.remove("navbar-fixed");
+    }
+});
+// show sidebar
+navBtn.addEventListener("click", function () {
+    sidebar.classList.add("show-sidebar");
+});
+closeBtn.addEventListener("click", function () {
+    sidebar.classList.remove("show-sidebar");
+});
+// set year
+date.innerHTML = new Date().getFullYear();
+
+
+var mybutton = document.getElementById("scrollTop");
+window.onscroll = function () { scrollFunction() };
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
